222355cd0c53457daade0f600c6f39e1e31487c6
Rachel Harris <rachel@fathom.info>
2018-02-06 12:19:09 -0500

    new updates

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 var table;
+var font;
+var yearoffset = 25;    // how far the year labels need to be pushed back
 var movies = [];
-var margin = 100;
-var lineheight = 800;
-var endpoint = 7;
-var gridspace = 130;
+var margin = 100;       // left and top edge
+var lineheight = 800;   // height of budget bars
+var gridspace = 130;    // space between budget bars
 var startyear = 2008;
-var r = 5;
 var hovered = false;
+var r = 5;              // scale of radius correlating to how many awards films have won
 
 
 var scalegap;
@@ -20,6 +21,7 @@ const GRIDMID = 480; // midpoint of vertical line grid
 
 function preload() {
   table = loadTable("budgets.csv", "csv", "header");
+  font = loadFont("fonts/apercu_mono.otf");
 }
 
 function mouseMoved(){
@@ -35,8 +37,9 @@ function mouseMoved(){
 
 
 function setup() {
-  createCanvas(1200,1000);
+  createCanvas(1900,1000);
   rectMode(CENTER);
+  textFont(font);
   currentscale = scale1;
   scalegap = lineheight / (currentscale.length-1);
   
@@ -52,7 +55,6 @@ function setup() {
     movie.y = map(movie.budget, 0, 250, GRIDMID + lineheight/2, GRIDMID - lineheight/2);
     movie.hovered = false;
     movies.push(movie);
-    console.log(movie.y);
   }
 
 }
@@ -67,42 +69,24 @@ function draw() {
 
   for (var g = 0; g < 11; g++){
         // DRAW GRID LINES
+        push();
+        stroke('#33323D');
+        strokeWeight(2);
+        noFill();
         line(margin + gridspace * g, GRIDMID + lineheight/2, margin + gridspace * g, GRIDMID - lineheight/2);
-        // ellipse(margin + gridspace * g, GRIDMID - lineheight/2 - endpoint/2, endpoint, endpoint);
-        // ellipse(margin + gridspace * g, GRIDMID + lineheight/2 + endpoint/2, endpoint, endpoint);
+        pop();
+  }
+  
+  for (var t = startyear; t < 2019; t++){
+        push();
+        fill(255);
+        noStroke();
+        textSize(20);
+        text(t, margin - yearoffset + gridspace * abs(startyear-t), 60);  
   }
 
 
-// // SUCKY ATTEMPT TO HIGHLIGHT NONWINNERS 
-//   for (var m = 0; m < movies.length; m++){ 
-//       // DRAW MOVIE DATA CIRCLES
-//     if (movies[m].date >= startyear){
-//       if (movies[m].best != "TRUE"){
-//         if (movies[m].hovered === false){
-//         fill(60, 60, 70, 170); 
-//         noStroke();
-//         ellipse(margin + gridspace * abs(startyear - movies[m].date), 
-//                   map(movies[m].budget, 0, 250, GRIDMID + lineheight/2, GRIDMID - lineheight/2),
-//                   r * movies[m].awards, r * movies[m].awards);
-//         }
-//       }
-//     }
-//   }      
-//   for (var m = 0; m < movies.length; m++){ 
-//       // DRAW MOVIE DATA CIRCLES
-//     if (movies[m].date >= startyear){
-//       if (movies[m].best != "TRUE"){
-//         if (movies[m].hovered === true){
-//         fill(70, 70, 77, 200);   
-//         noStroke();
-//         ellipse(margin + gridspace * abs(startyear - movies[m].date), 
-//                   map(movies[m].budget, 0, 250, GRIDMID + lineheight/2, GRIDMID - lineheight/2),
-//                   r * movies[m].awards, r * movies[m].awards);
-//         }
-//       }
-//     }
-//   }  
-// // END OF SUCKY ATTEMPT TO HIGHLIGHT NONWINNERS
+
 
   for (var m = 0; m < movies.length; m++){ 
       // DRAW MOVIE DATA CIRCLES
@@ -136,11 +120,48 @@ function draw() {
     }
   }
       
-  // for (var s = 0; s < currentscale.length + 1; s++){
-  //     textSize(14);
-  //     line(1100, GRIDMID + lineheight/2 - scalegap * s, 1120, GRIDMID + lineheight/2 - scalegap * s);    
-  //     // text(currentscale[s], 1100, GRIDMID + lineheight/2 - scalegap * s, 150, 20);
-  // } 
+      // for (var t = 0; t < 11; t++){      
+      // fill(255);
+      // noStroke();
+      // textSize(20);
+      // text("TEE", margin + gridspace * t, 100);  
+      // }
 
 
 }
+
+// // SUCKY ATTEMPT TO HIGHLIGHT NONWINNERS 
+//   for (var m = 0; m < movies.length; m++){ 
+//       // DRAW MOVIE DATA CIRCLES
+//     if (movies[m].date >= startyear){
+//       if (movies[m].best != "TRUE"){
+//         if (movies[m].hovered === false){
+//         fill(60, 60, 70, 170); 
+//         noStroke();
+//         ellipse(margin + gridspace * abs(startyear - movies[m].date), 
+//                   map(movies[m].budget, 0, 250, GRIDMID + lineheight/2, GRIDMID - lineheight/2),
+//                   r * movies[m].awards, r * movies[m].awards);
+//         }
+//       }
+//     }
+//   }      
+//   for (var m = 0; m < movies.length; m++){ 
+//       // DRAW MOVIE DATA CIRCLES
+//     if (movies[m].date >= startyear){
+//       if (movies[m].best != "TRUE"){
+//         if (movies[m].hovered === true){
+//         fill(70, 70, 77, 200);   
+//         noStroke();
+//         ellipse(margin + gridspace * abs(startyear - movies[m].date), 
+//                   map(movies[m].budget, 0, 250, GRIDMID + lineheight/2, GRIDMID - lineheight/2),
+//                   r * movies[m].awards, r * movies[m].awards);
+//         }
+//       }
+//     }
+//   }  
+// // END OF SUCKY ATTEMPT TO HIGHLIGHT NONWINNERS
+
+// LITTLE ENDPOINTS
+        // var endpoint = 7; (put this at top)
+        // ellipse(margin + gridspace * g, GRIDMID - lineheight/2 - endpoint/2, endpoint, endpoint);
+        // ellipse(margin + gridspace * g, GRIDMID + lineheight/2 + endpoint/2, endpoint, endpoint);
